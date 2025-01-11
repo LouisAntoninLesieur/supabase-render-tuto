198fe312e51444bdac42c495e5bf7486b44560f4
LouisAntoninLesieur <louisantonin.lesieur@icloud.com>
2025-01-11 15:51:30 +0100

    fix: add PORT variable and listen app on it

--- index.js
+++ index.js
@@ -11,7 +11,11 @@ app.use('/api', apiRouter.routes);
 
 // Example of listening on port 3000
 // Useful for testing your API endpoints on your local machine
-
 // app.listen(3000, () => {
 //   console.log('Lift-off ! 🚀 Your API is running on http://localhost:3000');
 // })
+
+const PORT = process.env.PORT || 3000;
+app.listen(process.env.PORT, () => {
+  console.log(`Lift-off ! 🚀 Your API is running on port ${PORT}`);
+})
